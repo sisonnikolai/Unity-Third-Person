fa52744435a8aca1f9fa729c5b2722c8d7ccad13
Nikolai Sison <sisonnikolai@gmail.com>
2022-10-23 11:59:44 +0800

    Add basic stamina UI

--- UI/UIManager.cs
+++ UI/UIManager.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI healthDisplay;
+    [SerializeField] private TextMeshProUGUI staminaDisplay;
 
     private GameObject player;
     private Health health;
+    private Stamina stamina;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         health = player.GetComponent<Health>();
+        stamina = player.GetComponent<Stamina>();
     }
 
     // Update is called once per frame
     void Update()
     {
         healthDisplay.text = health.GetValue.ToString();
+        staminaDisplay.text = stamina.GetValue.ToString("0.00");
     }
 }
